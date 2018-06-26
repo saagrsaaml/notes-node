11e22411211e36bbfc1b0e0de95fd77fb4a3d9aa
Sagar Samal <saagrsaaml@gmail.com>
2018-06-26 22:34:21 +0545

    append file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Starting app ....");
+
+const fs = require('fs');
+
+// fs.appendFile('greetings.txt', 'Hello World!!', function(err) {
+//   if(err){
+//     console.log('error');
+//   }
+// });
+
+// OR
+
+fs.appendFileSync('greetings.txt', 'Hello World!');
