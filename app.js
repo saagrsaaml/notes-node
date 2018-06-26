19590ce731c3c9ab0829e5546e6e8cd2ff09e249
Sagar Samal <saagrsaaml@gmail.com>
2018-06-27 01:01:56 +0545

    export properties.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-console.log("Starting app ....");
+console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
 var user = os.userInfo();
-console.log(user);
 
 // fs.appendFile('greetings.txt', 'Hello World!!', function(err) {
 //   if(err){
@@ -14,4 +14,4 @@ console.log(user);
 
 // OR
 
-// fs.appendFileSync('greetings.txt', 'Hello World!');
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
