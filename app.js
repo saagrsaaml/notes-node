51d85c45013c5d8da9b1ff90e91ee2fd185fda80
Sagar Samal <saagrsaaml@gmail.com>
2018-06-30 02:22:07 +0545

    using args to parse arguments for notes.

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ const argv = yargs.argv;
 const command = process.argv[2];
 
 console.log('Command: ', command);
-console.log('Process: ', process.argv);
 console.log('Yargs: ', argv);
 
 if (command === 'add') {
   notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes.')
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Fetching note.')
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing note.')
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized.')
 }
--- notes.js
+++ notes.js
@@ -4,6 +4,21 @@ var addNote = (title, body) => {
   console.log('Adding note', title, body);
 };
 
+var getAll = () => {
+  console.log('Getting all notes.')
+};
+
+var getNote = (title) => {
+  console.log('Getting notes',title)
+};
+
+var removeNote = (title) => {
+  console.log('Removing notes',title)
+};
+
 module.exports = {
-  addNote
+  addNote,
+  getAll: getAll,
+  getNote,
+  removeNote
 };
