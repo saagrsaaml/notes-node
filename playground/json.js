322e754b7f5c64b5badf4fa1d5db8a448250914e
Sagar Samal <saagrsaaml@gmail.com>
2018-06-30 09:21:15 +0545

    converting JSON string to object.

--- playground/json.js
+++ playground/json.js
@@ -1,9 +1,15 @@
-var obj = {
-  name: 'Sagar'
-};
-
+// var obj = {
+//   name: 'Sagar'
+// };
+//
 // converting js object into a string
-var stringObj = JSON.stringify(obj);
+// var stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj);
+// console.log(stringObj);
 
-console.log(typeof stringObj);
-console.log(stringObj);
+var personString = '{"name": "Sagar", "age": 25}';
+// takes JSON and converts string back into its original form which can be an array or object
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
