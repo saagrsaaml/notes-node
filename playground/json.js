3ed564b9700ea8eb5f00f00597f8c44c03b32921
Sagar Samal <saagrsaaml@gmail.com>
2018-06-30 09:31:06 +0545

    working with JSON.

--- playground/json.js
+++ playground/json.js
@@ -8,8 +8,25 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"name": "Sagar", "age": 25}';
+// var personString = '{"name": "Sagar", "age": 25}';
 // takes JSON and converts string back into its original form which can be an array or object
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+}
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString)
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof originalNoteString);
+console.log(typeof note);
+console.log(note.title);
