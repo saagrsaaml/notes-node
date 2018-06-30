9362ce566539ae4ae041a789b16adaf5889aeabb
Sagar Samal <saagrsaaml@gmail.com>
2018-06-30 10:00:14 +0545

    adding and saving notes

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    // reads notes and parses it into JSON object if notes-data.json exists.
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }  
 };
 
 var getAll = () => {
